afda64b61eeb1704f15a40295bd8aae2ebe65f47
YashNK <yashkamnani0@gmail.com>
2023-06-28 18:43:31 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components"
+
 
 const App = () => {
 
   return (
     <>
-      <div>Fuck My Life</div> 
+      <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cove-bg-no-repeat bg-center">
+            <Navbar />
+            <Hero />
+          </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+          <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+          </div>
+        </div>
+      </BrowserRouter>
     </>
   )
 }
